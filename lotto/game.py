236029bd06282e7c3531857abb0e1fb6a3c82c82
Maxim Maslin <zapzarap@yandex.ru>
2019-12-22 14:04:34 +0300

    update game

--- lotto/game.py
+++ lotto/game.py
@@ -1,11 +1,135 @@
 import random
-import time
+from itertools import chain
 
 
 class NumPlayersException(Exception):
     def __init__(self, message, errors):
         super().__init__(message)
         self.errors = errors
+
+
+class Singleton:
+    __instance = None
+
+    def __init__(self):
+        if Singleton.__instance:
+            print(f'Экземпляр уже создан: {self.get_instance()}')
+
+    @classmethod
+    def get_instance(cls):
+        if not cls.__instance:
+            cls.__instance = Singleton()
+        return cls.__instance
+
+
+class Card:
+    def __init__(self, first_row, second_row, last_row):
+        self.first_row = first_row
+        self.second_row = second_row
+        self.last_row = last_row
+
+    def __eq__(self, other):
+        return all([
+            self.first_row == other.first_row,
+            self.second_row == other.second_row,
+            self.last_row == other.last_row
+        ])
+
+    def update(self, barrel):
+        self.first_row = [None if x == barrel.digit else x for x in self.first_row]
+        self.second_row = [None if x == barrel.digit else x for x in self.second_row]
+        self.last_row = [None if x == barrel.digit else x for x in self.last_row]
+
+    def __iter__(self):
+        return (x for x in chain(self.first_row, self.second_row, self.last_row))
+
+    @property
+    def is_complete(self):
+        return any([
+            all(self.first_row), all(self.second_row), all(self.last_row)
+        ])
+
+    def __str__(self):
+        return '\n'.join([
+            ' '.join(str(x) if x else 'X' for x in row)
+            for row in (self.first_row, self.second_row, self.last_row)
+        ])
+
+
+class CardGenerator(Singleton):
+    _cards = []
+
+    @staticmethod
+    def _get_use_digit_in_first_column(probability=2/3):
+        return random.random() < probability
+
+    @staticmethod
+    def _generate_row_digits(
+            previous_rows, first_digit=None, last_card=False
+    ):
+        end_of_range = 90 if last_card else 89
+        digits = [first_digit] if first_digit else []
+        previous_rows = previous_rows or []
+        amount_of_digits_in_row = 5
+        while len(digits) < amount_of_digits_in_row:
+            digit = random.randint(10, end_of_range)
+            if digit not in digits and digit not in previous_rows:
+                digits.append(digit)
+        return digits
+
+    @classmethod
+    def _generate_row(cls, previous_rows, last=False):
+        row = []
+        use_digit_in_first_column = cls._get_use_digit_in_first_column()
+        if use_digit_in_first_column:
+            first_digit = random.randint(1, 9)
+            row_digits = cls._generate_row_digits(
+                previous_rows,
+                first_digit=first_digit,
+                last_card=last
+            )
+            row.extend(row_digits)
+        else:
+            row.extend(cls._generate_row_digits(previous_rows, last_card=last))
+        row = sorted(row)
+        nones = [None] * 4
+        row.extend(nones)
+        return row
+
+    @classmethod
+    def _generate_card(cls):
+        first_row = cls._generate_row(None)
+        second_row = cls._generate_row(first_row)
+        previous_rows = first_row + second_row
+        last_row = cls._generate_row(previous_rows, last=True)
+        return Card(first_row, second_row, last_row)
+
+    @classmethod
+    def generate_cards(cls, num_players):
+        while len(cls._cards) < num_players:
+            card = cls._generate_card()
+            if card not in cls._cards:
+                cls._cards.append(card)
+        return cls._cards
+
+    @staticmethod
+    def _shuffle_card_row(row):
+        if row[0] < 10:
+            return [row[0]] + random.sample(row[1:], len(row) - 1)
+        row.remove(None)
+        return [None] + random.sample(row, len(row))
+
+    @classmethod
+    def give_card(cls):
+        try:
+            card = cls._cards.pop()
+        except IndexError:
+            return None
+        else:
+            card.first_row = cls._shuffle_card_row(card.first_row)
+            card.second_row = cls._shuffle_card_row(card.second_row)
+            card.last_row = cls._shuffle_card_row(card.last_row)
+            return card
 
 
 class Player:
@@ -25,65 +149,12 @@ class Barrel:
         return f'Бочонок с цифрой {self.digit}'
 
 
-class Card:
-    num_digits = 15
-
-    def __init__(self):
-        self.digits = Card._generate_digits(self)
-
-    @staticmethod
-    def _generate_digits(cls):
-        digits = {}
-        while len(digits) < 15:
-            digit = random.randint(1, 90)
-            digits[digit] = False
-        return digits
-
-    def update(self, digit):
-        self.digits[digit] = True
-
-    @property
-    def is_complete(self):
-        return all(self.digits.values())
-
-    def __str__(self):
-        card = [
-            f'({digit})' if self.digits[digit] else str(digit)
-            for digit in sorted(self.digits)
-        ]
-        card = [' '.join(x) for x in ([card[0:5], card[5:10], card[10:15]])]
-        return f'Цифры на его карточке:\n{card[0]}\n{card[1]}\n{card[2]}'
-
-
-class Sack:
-    num_barrels = 90
-
-    def __init__(self):
-        self.barrels = Sack._fill_with_barrels(self)
-
-    @staticmethod
-    def _fill_with_barrels(cls):
-        barrels = [Barrel(i) for i in range(1, cls.num_barrels + 1)]
-        random.shuffle(barrels)
-        return barrels
-
-    def pick_barrel(self):
-        return self.barrels.pop(random.randrange(len(self.barrels)))
-
-    def __iter__(self):
-        return self.barrels
-
-    def __str__(self):
-        barrels_digits = ' '.join(
-            map(str, [barrel.digit for barrel in self.barrels])
-        )
-        return f'Содержимое мешочка: {barrels_digits}'
-
-
 def get_num_players():
     while True:
         try:
-            num_players = int(input('Введите количество игроков (от 2 до 6): '))
+            num_players = int(
+                input('Введите количество игроков (от 2 до 6): ')
+            )
             if num_players < 2 or num_players > 6:
                 raise NumPlayersException(
                     'Количество игроков должно быть от 2 до 6',
@@ -95,38 +166,112 @@ def get_num_players():
             return num_players
 
 
-def get_players(num_players):
-    players = []
-    for i in range(1, num_players + 1):
-        player_name = input(f'Введите имя игрока {i}: ')
-        player_card = Card()
-        player = Player(player_name, player_card)
-        players.append(player)
-    return players
+def generate_player(i, card):
+    while True:
+        try:
+            player_name = input(f'Введите имя игрока {i}: ')
+        except UnicodeDecodeError:
+            print('Ошибка распознавания ввода, повторие ввод')
+        else:
+            break
+    return Player(player_name, card)
+
+
+class Sack(Singleton):
+    num_barrels = 90
+
+    def __init__(self):
+        super().__init__()
+        self.barrels = self._fill_with_barrels()
+
+    @classmethod
+    def _fill_with_barrels(cls):
+        barrels = [Barrel(i) for i in range(1, cls.num_barrels + 1)]
+        random.shuffle(barrels)
+        return barrels
+
+    def __iter__(self):
+        return (x for x in self.barrels)
+
+    def __str__(self):
+        barrels_digits = ' '.join(
+            map(str, [barrel.digit for barrel in self.barrels])
+        )
+        return f'Содержимое мешочка: {barrels_digits}'
+
+
+class Host(Singleton):
+    def __init__(self, sack, generator, num_cards=24):
+        super().__init__()
+        self.sack = sack
+        self.generator = generator
+        self.generator.generate_cards(num_cards)
+
+    def pick_barrel(self):
+        try:
+            return self.sack.barrels.pop(
+                random.randrange(len(self.sack.barrels))
+            )
+        except IndexError:
+            print('В мешочке больше нет бочонков')
+            return None
+
+    def give_card(self):
+        return self.generator.give_card()
+
+    @staticmethod
+    def check_card(player, barrel):
+        return barrel.digit in chain(
+            player.card.first_row, player.card.second_row, player.card.last_row
+        )
+
+    @staticmethod
+    def ask_player(player, barrel):
+        while True:
+            print(f'{player}, на вашей карточке есть цифра {barrel.digit}?')
+            print('У вас такая карточка:')
+            print(player.card)
+            try:
+                player_answer = input('да/нет: ').lower()
+            except UnicodeDecodeError:
+                print('Ошибка распознавания ввода, повторите ввод')
+                continue
+            if player_answer not in ['да', 'нет']:
+                print('Пожалуйста, ответьте "да" или "нет"')
+            else:
+                return player_answer
 
 
 def main():
     num_players = get_num_players()
-    players = get_players(num_players)
     sack = Sack()
-    print()
-    i = 0
-    while sack.barrels:
-        for picker in players:
-            barrel = sack.pick_barrel()
-            i += 1
-            print(f'{picker} берет {barrel}')
-            for player in players:
-                if barrel.digit in player.card.digits:
-                    print(f'Цифра на бочонке есть в карточке {player}')
-                    player.card.update(barrel.digit)
-                    print(player.card)
+    card_generator = CardGenerator()
+    host = Host(sack, card_generator, num_players)
+    players = [
+        generate_player(i, host.give_card()) for i in range(1, num_players + 1)
+    ]
+    while host.sack.barrels:
+        barrel = host.pick_barrel()
+        for player in players[:]:
+            player_answer = host.ask_player(player, barrel)
+            digit_on_card = host.check_card(player, barrel)
+            if player_answer == 'да':
+                if digit_on_card:
+                    print(f'{player}, обновляю вашу карточку')
+                    player.card.update(barrel)
                     if player.card.is_complete:
-                        print(f'{player} выиграл на {i} бочонке!')
+                        print(f'{player} выиграл!')
                         return
+                else:
+                    print(f'{player} ошибся, у него есть такая цифра. Игрок выбывает из игры')
+                    players.remove(player)
+            else:
+                if digit_on_card:
+                    print(f'{player} ошибся, у него нет такой цифры. Игрок выбывает из игры')
+                    players.remove(player)
             print()
-            time.sleep(1)
-            print()
+    else:
+        print('Ничья')
 
 
 if __name__ == '__main__':
