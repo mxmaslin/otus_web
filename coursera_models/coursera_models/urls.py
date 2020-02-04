1f93899a53552e8797474cb8163b72d65fb01ecf
Maxim Maslin <zapzarap@yandex.ru>
2020-02-04 08:06:56 +0300

    remove redundant

--- coursera_models/coursera_models/urls.py
+++ coursera_models/coursera_models/urls.py
@@ -4,6 +4,5 @@ from django.urls import include, path, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('profiles/', include('django.contrib.auth.urls')),
     re_path('^', include('courses.urls', namespace='courses')),
 ]
