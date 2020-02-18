d158f5536976538e71d359de940eb4d582715ef1
Maxim Maslin <zapzarap@yandex.ru>
2020-02-18 18:34:09 +0300

    add main.py

---
+++ flask_tasks/blog_final/main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/register/', methods=['GET'])
+def register():
+    return render_template('register.html')
+
+
+@app.route('/register/', methods=['POST'])
+def register_post():
+    return render_template('register.html')
+
+
+if __name__ == '__main__':
+    app.run()
