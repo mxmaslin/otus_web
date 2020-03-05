205d4b9eee7635df7541acb3aa0f6486176890f4
Maxim Maslin <zapzarap@yandex.ru>
2020-03-05 12:30:47 +0300

    implement registering new users

--- coursera/coursera_api/profiles/urls.py
+++ coursera/coursera_api/profiles/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from . import views
+from . import views, api_views
 
 app_name = 'profiles'
 
 urlpatterns = [
     path('student-signup/', views.StudentSignUp.as_view(), name='student-signup'),
     path('teacher-signup/', views.TeacherSignUp.as_view(), name='teacher-signup'),
+
+    path('v1/register-student/', api_views.register_student,
+         name='register-student-api'),
+    path('v1/register-teacher/', api_views.register_teacher,
+         name='register-teacher-api'),
 ]
