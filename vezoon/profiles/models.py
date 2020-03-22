4b71ed9a8f07382f1976de5c4161c81dbd4809a8
Maxim Maslin <zapzarap@yandex.ru>
2020-03-22 20:10:56 +0300

    add user model

--- vezoon/profiles/models.py
+++ vezoon/profiles/models.py
@@ -1,12 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
-class Passenger(models.Model):
+class User(AbstractUser):
+    pass
+
+
+class Passenger(User):
     pass
 # email, телефон, first_name, last_name, фото, **оценки**
 
 
-class Driver(models.Model):
+class Driver(User):
     pass
 # email, телефон, first_name, last_name, фото, **оценки **, **автомобиль**
 
