87ce6ab1d2105fac1d2d301139c464cacb5f9a31
Maxim Maslin <zapzarap@yandex.ru>
2020-05-04 23:09:41 +0300

    add new urls

--- coursera/coursera_react1/frontend/src/config.js
+++ coursera/coursera_react1/frontend/src/config.js
@@ -9,10 +9,17 @@ const jquery = path.join(staticPath, 'vendors/jquery/jquery-3.4.1.min.js');
 
 
 const baseUrl = 'http://127.0.0.1:8000';
-const userUrl = url.resolve(baseUrl, 'profiles-api/v2/user');
-const loginUrl = url.resolve(baseUrl, 'auth/login/?next=/profiles/student-signup');
-const studentSignupUrl = url.resolve(baseUrl, 'profiles/student-signup');
-const teacherSignupUrl = url.resolve(baseUrl, 'profiles/teacher-signup');
+const userUrl = url.resolve(baseUrl, 'profiles-api/v2/user/');
+const loginUrl = url.resolve(baseUrl, 'auth/login/?next=/profiles/student-signup/');
+const studentSignupUrl = url.resolve(baseUrl, 'profiles/student-signup/');
+const teacherSignupUrl = url.resolve(baseUrl, 'profiles/teacher-signup/');
+const logoutUrl = url.resolve(baseUrl, 'auth/logout/');
+const adminUrl = url.resolve(baseUrl, 'admin/');
+const feedbackUrl = url.resolve(baseUrl, 'feedback/');
+const myCoursesUrl = url.resolve(baseUrl, 'my-courses/');
+const lecturingUrl = url.resolve(baseUrl, 'lecturing/');
+const createUrl = url.resolve(baseUrl, 'create/');
+
 
 module.exports = {
     bootstrapCss,
@@ -22,5 +29,11 @@ module.exports = {
     userUrl,
     loginUrl,
     studentSignupUrl,
-    teacherSignupUrl
+    teacherSignupUrl,
+    logoutUrl,
+    adminUrl,
+    feedbackUrl,
+    myCoursesUrl,
+    lecturingUrl,
+    createUrl
 };
