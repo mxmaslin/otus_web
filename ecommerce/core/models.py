31077276dcdff9d06ffd20602f67727c473a2d03
Maxim Maslin <zapzarap@yandex.ru>
2020-06-02 22:56:17 +0300

    add Item/OrderItem/Order models + migrations

--- ecommerce/core/models.py
+++ ecommerce/core/models.py
@@ -1,2 +1,28 @@
+from django.conf import settings
 from django.db import models
 
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.title
+
+
+class OrderItem(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.item
+
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
