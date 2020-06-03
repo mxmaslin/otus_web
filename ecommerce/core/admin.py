772cabfd8e642a77a488dd8655e6353d33e540b0
Maxim Maslin <zapzarap@yandex.ru>
2020-06-03 12:23:30 +0300

    add admin

--- ecommerce/core/admin.py
+++ ecommerce/core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Item, OrderItem, Order
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
