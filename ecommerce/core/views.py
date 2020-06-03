35372a0f9fc3c528f627ed04181539a8202ffaf6
Maxim Maslin <zapzarap@yandex.ru>
2020-06-03 12:23:59 +0300

    add item view

--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Item
+
+
+def item_list(request):
+    context = {
+        'items': Item.objects.all()
+    }
+    return render(request, 'home-page.html', context)
+
+
