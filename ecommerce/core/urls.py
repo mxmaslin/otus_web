06d1d1983b51b7d1575db693410b61305474cd91
Maxim Maslin <zapzarap@yandex.ru>
2020-06-07 22:53:02 +0300

    add add_to_card view

--- ecommerce/core/urls.py
+++ ecommerce/core/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (
     HomeView,
     ItemDetailView,
+    add_to_cart,
     checkout
 )
 
@@ -10,5 +11,6 @@ app_name = 'core'
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
+    path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
     path('checkout/<slug>/', checkout, name='checkout'),
 ]
