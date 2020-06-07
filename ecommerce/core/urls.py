6f62acf73af3f895b4c7cd724197d22934e0ef88
Maxim Maslin <zapzarap@yandex.ru>
2020-06-07 13:20:19 +0300

    use view class

--- ecommerce/core/urls.py
+++ ecommerce/core/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import item_list
+from .views import (
+    HomeView,
+    ItemDetailView,
+    products,
+    checkout
+)
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', item_list, name='item-list')
+    path('', HomeView.as_view(), name='home'),
+    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
+    path('checkout/<slug>/', checkout, name='checkout'),
 ]
--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from .models import Item
 
 
-def item_list(request):
+def products(request):
     context = {
         'items': Item.objects.all()
     }
-    return render(request, 'home-page.html', context)
+    return render(request, 'products.html', context)
 
+
+def checkout(request):
+    return render(request, 'checkout.html')
+
+
+class HomeView(ListView):
+    model = Item
+    template_name = 'home-page.html'
+
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = 'product-page.html'
 
