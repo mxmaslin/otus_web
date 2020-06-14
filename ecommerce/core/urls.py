a9949fbfc671b197b76ed0ca37c91c2e97857a76
Maxim Maslin <zapzarap@yandex.ru>
2020-06-14 22:01:24 +0300

    update urls

--- ecommerce/core/urls.py
+++ ecommerce/core/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
     path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
-    path('remove-item-from-cart/',
+    path('remove-item-from-cart/<slug>/',
          remove_single_item_from_cart,
-         name='remove_single_item_from_cart')
+         name='remove-single-item-from-cart')
 ]
