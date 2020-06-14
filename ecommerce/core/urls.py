94b5fc0dfb1da2c988d3f3b5ff343b9f2c49acb9
Maxim Maslin <zapzarap@yandex.ru>
2020-06-14 21:09:51 +0300

    add order summary page

--- ecommerce/core/urls.py
+++ ecommerce/core/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (
     HomeView,
     ItemDetailView,
+    OrderSummaryView,
     add_to_cart,
     remove_from_cart,
     checkout
@@ -12,6 +13,7 @@ app_name = 'core'
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
+    path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
     path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
     path('ceckout/<slug>/', checkout, name='checkout'),
