e5d1c274d16d2fa8a531c9382e5ec43a8f82f5eb
Maxim Maslin <zapzarap@yandex.ru>
2020-06-14 22:01:13 +0300

    fix order count in navbar

--- ecommerce/core/templatetags/cart_template_tags.py
+++ ecommerce/core/templatetags/cart_template_tags.py
@@ -9,5 +9,5 @@ def cart_item_count(user):
     if user.is_authenticated:
         qs = Order.objects.filter(user=user, ordered=False)
         if qs.exists():
-            return qs[0].items.count()
+            return sum(item.quantity for item in qs[0].items.all())
     return 0
