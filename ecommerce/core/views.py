d289c192193819edf4dfb2191cdea48e4686879e
Maxim Maslin <zapzarap@yandex.ru>
2020-06-14 21:09:29 +0300

    add OrderSummaryView, rewrite add/remove items functions

--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -1,7 +1,10 @@
 from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils import timezone
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 
 from .models import Item, OrderItem, Order
 
@@ -17,10 +20,24 @@ class ItemDetailView(DetailView):
     template_name = 'product-page.html'
 
 
+class OrderSummaryView(LoginRequiredMixin, View):
+    def get(self, *args, **kwargs):
+        try:
+            order = Order.objects.get(user=self.request.user, ordered=False)
+            context = {'object': order}
+            return render(self.request, 'order-summary.html', context)
+        except ObjectDoesNotExist:
+            messages.error(self.request, 'У вас нет активных заказов')
+            return redirect('/')
+
+
+@login_required
 def add_to_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
     order_item, created = OrderItem.objects.get_or_create(
-        item=item, user=request.user
+        item=item,
+        user=request.user,
+        ordered=False
     )
     order_qs = Order.objects.filter(user=request.user, ordered=False)
     if order_qs.exists():
@@ -28,47 +45,76 @@ def add_to_cart(request, slug):
         if order.items.filter(item__slug=item.slug).exists():
             order_item.quantity += 1
             order_item.save()
-            messages.info(request, 'В корзину добавлен ещё один экземпляр')
-            return redirect('core:product', slug=slug)
+            messages.info(request, 'Количество этого товара было обновлено.')
+            return redirect("core:order-summary")
         else:
             order.items.add(order_item)
-            messages.info(request, 'Этот товар был добавлен в вашу корзину')
-            return redirect('core:product', slug=slug)
+            messages.info(request, 'Этот товар был добавлен в вашу корзину.')
+            return redirect("core:order-summary")
     else:
         ordered_date = timezone.now()
         order = Order.objects.create(
-            user=request.user, ordered_date=ordered_date
-        )
+            user=request.user, ordered_date=ordered_date)
         order.items.add(order_item)
-        messages.info(request, 'Этот товар был добавлен в вашу корзину')
-        return redirect('core:product', slug=slug)
-    return redirect('core:product', slug=slug)
+        messages.info(request, 'Этот товар был добавлен в вашу корзину.')
+        return redirect("core:order-summary")
 
 
+@login_required
 def remove_from_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
-    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False
+    )
     if order_qs.exists():
         order = order_qs[0]
         if order.items.filter(item__slug=item.slug).exists():
             order_item = OrderItem.objects.filter(
                 item=item,
-                user=request.user
+                user=request.user,
+                ordered=False
+            )[0]
+            order.items.remove(order_item)
+            order_item.delete()
+            messages.info(request, 'Этот товар был удалён из вашей корзины.')
+            return redirect("core:order-summary")
+        else:
+            messages.info(request, 'Этого товара не было в вашей корзине.')
+            return redirect("core:product", slug=slug)
+    else:
+        messages.info(request, 'У вас нет активных заказов.')
+        return redirect("core:product", slug=slug)
+
+
+@login_required
+def remove_single_item_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False
+    )
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
             )[0]
             if order_item.quantity > 1:
                 order_item.quantity -= 1
                 order_item.save()
             else:
                 order.items.remove(order_item)
-                order_item.delete()
-            messages.info(request, 'Этот товар был удалён из корзины')
+            messages.info(request, 'Количество этого товара было обновлено.')
+            return redirect("core:order-summary")
         else:
-            messages.info(request, 'Этого товара не было в вашей корзине')
-            return redirect('core:product', slug=slug)
+            messages.info(request, 'Этого товара не было в вашей корзине.')
+            return redirect("core:product", slug=slug)
     else:
-        messages.info(request, 'У вас пока нет заказов')
-        return redirect('core:product', slug=slug)
-    return redirect('core:product', slug=slug)
+        messages.info(request, 'У вас нет активных заказов.')
+        return redirect("core:product", slug=slug)
 
 
 def checkout(request):
