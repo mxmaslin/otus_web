c86d6794334da8d696c9b8a069b138f438984c7f
Maxim Maslin <zapzarap@yandex.ru>
2020-06-14 21:16:28 +0300

    add remove item from cart url

--- ecommerce/core/urls.py
+++ ecommerce/core/urls.py
@@ -1,20 +1,24 @@
 from django.urls import path
 from .views import (
     HomeView,
+    checkout,
     ItemDetailView,
     OrderSummaryView,
     add_to_cart,
     remove_from_cart,
-    checkout
+    remove_single_item_from_cart
 )
 
 app_name = 'core'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
+    path('checkout/', checkout, name='checkout'),
     path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
+    path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
     path('remove-from-cart/<slug>/', remove_from_cart, name='remove-from-cart'),
-    path('ceckout/<slug>/', checkout, name='checkout'),
+    path('remove-item-from-cart/',
+         remove_single_item_from_cart,
+         name='remove_single_item_from_cart')
 ]
