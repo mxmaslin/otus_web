dc4001f11a58be8381b9627dec9bd4c67b842aab
Maxim Maslin <zapzarap@yandex.ru>
2020-06-27 17:52:42 +0300

    implement using django-environ

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -1,10 +1,12 @@
 import os
+import environ
 
-ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
+env = environ.Env()
+environ.Env.read_env()
 
-DEBUG = True
+DEBUG = env.bool('DEBUG')
+SECRET_KEY = env.str('SECRET_KEY')
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
@@ -74,24 +76,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, 'db.sqlite3')
-    }
-}
+DATABASES = {'default': env.db('DATABASE_URL')}
 
-if ENVIRONMENT == 'production':
-    DEBUG = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    SESSION_COOKIE_SECURE = True
-    SECURE_BROWSER_XSS_FILTER = True
-    SECURE_CONTENT_TYPE_NOSNIFF = True
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_SECONDS = 31536000
-    SECURE_REDIRECT_EXEMPT = []
-    SECURE_SSL_REDIRECT = True
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = '/'
